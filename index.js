7894ceabf561f6df0c2bda28824eb1b348032131
Deepak Sharma <uddibhardwaj08@gmail.com>
2023-01-30 22:19:27 +0530

    m

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ const a = fs.writeFileSync('test.txt', 'hello world');
 const b = fs.readFileSync('test.txt', 'utf-8');
 console.log(b)
 fs.renameSync('test.txt', 'mytest.txt')
+fs.appendFileSync('mytest.txt', 'my name is deepak sharma')
